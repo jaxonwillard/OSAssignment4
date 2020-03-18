d38aac97a142b300dc836bf6cb8e6f8cf428aa8b
Jaxon Willard <jaxonbwillard@gmail.com>
2020-03-18 18:51:13 -0400

    moving to linux

--- src/PI.java
+++ src/PI.java
@@ -1,10 +1,8 @@
-import javafx.concurrent.Task;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.concurrent.Semaphore;
 
 public class PI {
     volatile TaskQueue taskQueue;
@@ -15,35 +13,33 @@ public class PI {
     }
 
     public static void main(String[] args) {
-        for (int i=0; i<5; i++) {
             try {
-                PI piCalculator = new PI(i * 200);
+                PI piCalculator = new PI(1000);
                 ArrayList<Thread> threads = new ArrayList<>();
                 long startTime = System.currentTimeMillis();
                 for (int j = 0;  j< piCalculator.processors; j++) {
                     threads.add(new Thread(new WorkerThread(piCalculator.taskQueue, piCalculator.resultTable)));
                     threads.get(j).start();
-                    threads.get(j).join();
                 }
-                long totalTime = System.currentTimeMillis() - startTime;
-
-                System.out.println("size: " + i*200 + " totalTime: " + totalTime);
-//            for (int i=1; i<10; i++){
-//                System.out.println(piCalculator.resultTable.results.get(i));
-//            }
-
-
+                for (int j = 0; j<piCalculator.processors; j++){
+                    threads.get(j).join();
+                }long totalTime = System.currentTimeMillis() - startTime;
+                System.out.println();
+                for (int i=1; i<1000; i++){
+                    System.out.print(piCalculator.resultTable.getVal(i));
+                }
+                System.out.printf("\nPi Computation took %d ms", totalTime);
             } catch (Exception e) {
                 System.out.println(e);
             }
-        }
+
     }
 }
 
 class TaskQueue{
-    public Semaphore lock = new Semaphore(1);
     volatile LinkedList<Integer> linkedList;
     int size;
+    int index = 0;
     TaskQueue(int size){
         this.size = size;
         this.linkedList = getShuffledLL();
@@ -57,29 +53,31 @@ class TaskQueue{
         return linkedList;
     }
 
-    int getTask() throws InterruptedException {
-        this.lock.acquire();
-        int task = this.linkedList.pop();
-        this.lock.release();
-        return task;
+    synchronized int getTask()  {
+        index++;
+        if (index % 10 ==0){
+            System.out.flush();
+            System.out.print(".");
+        }
+        return this.linkedList.pop();
     }
-
-
 }
 
 class ResultTable{
-    public Semaphore lock = new Semaphore(1);
-    volatile HashMap<Integer, String> results = new HashMap<>();
-    void putKeyVal(int key, String value) throws InterruptedException {
-        this.lock.acquire();
+    private volatile HashMap<Integer, String> results = new HashMap<>();
+    synchronized void putKeyVal(int key, String value)  {
+        if (this.results.containsKey(key)){
+            System.out.println("i already have a key!");}
         this.results.put(key, value);
-        this.lock.release();
+    }
+    public String getVal(Integer key){
+        return this.results.get(key);
     }
 }
 
 class WorkerThread implements Runnable{
-    private TaskQueue taskQueue;
-    private ResultTable resultTable;
+    private volatile TaskQueue taskQueue;
+    private volatile ResultTable resultTable;
     Bpp bpp = new Bpp();
 
     WorkerThread(TaskQueue tq, ResultTable rt){
@@ -90,20 +88,11 @@ class WorkerThread implements Runnable{
     @Override
     public void run(){
             while(!this.taskQueue.linkedList.isEmpty()) {
-                int key = 0;
-                try {
-                    key = this.taskQueue.getTask();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                int key = this.taskQueue.getTask();
                 Integer value = this.bpp.getDecimal(key);
                 String valStr = String.valueOf(value).substring(0,1);
-                try {
-                    this.resultTable.putKeyVal(key, valStr);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-        }
+                this.resultTable.putKeyVal(key, valStr);
+            }
     }
 
 }
